09b10e4b8c9bc586946a1c4f93eee4d87606d8b5
Arthur-Kroth <akposselt@gmail.com>
2026-06-05 16:35:56 -0300

    feat: Add a check() method to verify if the matrices parameters are compatible with the addition and subtraction operations.

--- src/main/java/org/model/Operations.java
+++ src/main/java/org/model/Operations.java
@@ -22,6 +22,16 @@ public class Operations {
         }
     }
 
+    public void check() {
+        if (matrixA.length < 1 || matrixB.length < 1) {
+            throw new IllegalArgumentException("Matrices cannot have fewer than 1 row.");
+        } else if (matrixA[0].length < 1 || matrixB[0].length < 1) {
+            throw new IllegalArgumentException("Matrices cannot have fewer than 1 column.");
+        } else if (matrixA.length != matrixB.length || matrixA[0].length != matrixB[0].length) {
+            throw new IllegalArgumentException("The matrices must have the same number of rows and columns.");
+        }
+    }
+
     public double[][] add() {
         if (matrixA.length < 1 || matrixB.length < 1) {
             throw new IllegalArgumentException("Matrices cannot have fewer than 1 row.");
