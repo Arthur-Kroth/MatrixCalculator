f9a283390f6f3593168177974798a0ca01fbcb50
Arthur-Kroth <akposselt@gmail.com>
2026-06-05 22:30:22 -0300

    feat: add multiply() method.

--- src/main/java/org/model/Operations.java
+++ src/main/java/org/model/Operations.java
@@ -57,4 +57,25 @@ public class Operations {
 
         return result;
     }
+
+    public double[][] multiply(double[][] matrix1, double[][] matrix2) {
+        if (matrix1.length < 1 || matrix2.length < 1) {
+            throw new IllegalArgumentException("Matrices cannot have fewer than 1 row.");
+        } else if (matrix1[0].length < 1 || matrix2[0].length < 1) {
+            throw new IllegalArgumentException("Matrices cannot have fewer than 1 column.");
+        } else if (matrix1[0].length != matrix2.length) {
+            throw new IllegalArgumentException("The number of columns in the first matrix must be equal to the number of rows in the second matrix");
+        }
+
+        double[][] result = new double[matrix1.length][matrix2[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < matrix1[0].length; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
 }
